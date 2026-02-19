9b1f57e3577481814f8442a12a26e72844c211a4
tacopoor <tacopoor@gmail.com>
2026-02-19 17:44:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "futsal-tracker-cache-v3";
+const CACHE_NAME = "futsal-tracker-cache-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
